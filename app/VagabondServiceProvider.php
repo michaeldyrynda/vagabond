2d4e036613cdb01779ba2581d78c1090cc59fbcb
Michael Dyrynda <michael@dyrynda.com.au>
2018-01-18 19:34:26 +1030

    update service provider class name

--- app/VagabondServiceProvider.php
+++ app/VagabondServiceProvider.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Support\ServiceProvider;
 
-class MigrationServiceProvider extends ServiceProvider
+class VagabondServiceProvider extends ServiceProvider
 {
     public function boot()
     {
