a73800fe1f66b63465a65ed49a54330fddafd86d
Michael Dyrynda <michael@dyrynda.com.au>
2018-04-09 23:33:48 +0930

    make VagabondServiceProvider abstract for extension per-connection

--- app/VagabondServiceProvider.php
+++ app/VagabondServiceProvider.php
@@ -4,10 +4,36 @@ namespace App;
 
 use Illuminate\Support\ServiceProvider;
 
-class VagabondServiceProvider extends ServiceProvider
+abstract class VagabondServiceProvider extends ServiceProvider
 {
+    protected $connectionName;
+
     public function boot()
     {
-        $this->loadMigrationsFrom(dirname(__FILE__).'/../database/migrations');
+        if (is_dir($migrationPath = $this->migrationPath())) {
+            $this->loadMigrationsFrom($migrationPath);
+        }
+    }
+
+    public function register()
+    {
+        if (is_file($configPath = $this->configPath())) {
+            $this->mergeConfigFrom($configPath, $this->configKey());
+        }
+    }
+
+    protected function migrationPath()
+    {
+        return dirname(__FILE__)."/../../database/migrations/{$this->connectionName}";
+    }
+
+    protected function configPath()
+    {
+        return dirname(__FILE__)."/../../config/{$this->connectionName}.php";
+    }
+
+    protected function configKey($prefix = 'database.connections')
+    {
+        return "{$prefix}.{$this->connectionName}";
     }
 }
